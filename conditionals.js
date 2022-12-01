488ac7047bd36678435686f0ba5552a595de291d
dorreb <ashley_ure@hotmail.com>
2022-12-01 04:20:41 -0500

    Practice declaring and calling Functions

--- conditionals.js
+++ conditionals.js
@@ -20,7 +20,7 @@ if (temperature < 0){
 console.log("Now you're ready to go outside");
 
 const isCitizen = true;
-const age = 26;
+const age = 26; 
 if (isCitizen && age > 18){
   console.log("You are eligible to vote.");
 }
